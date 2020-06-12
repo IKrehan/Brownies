93a8ce1e7dabf88b36652f32e38505cdc64040a0
IKrehan <irvigkrehan@hotmail.com>
2020-06-12 18:17:51 -0300

    PH Brownies Site

--- Server/auth.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint, render_template, redirect, request, flash, url_for
-
-
-auth= Blueprint('auth', __name__)
--- Server/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SECRET_KEY = "GFBFhsBkZYv8jG9tNrFH6ywVjUHb8w5wsDg2czNWg676aMbNwuK8apSZmSDK6nXUr96NHKbwFnZyhaA3et8jyMy9qNAbrQb83jEQ4sk4R6ZWjXfM8Kv9Y3ce6QPGwyCgrHbctwZhLyavz8AjRdvWdrhCEhch9kCh2ZLPKDMgcnFhjgyT54BXtR7cSCWprzMrgDWeep47sMapYJ86XERjAUfWwvKYtEHYnRtxn5frPmeXgHptyzd5d3nAJjjDYxpU"
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'database.db')
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
