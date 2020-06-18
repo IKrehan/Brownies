36cb3e3a67c7c3b10a3f81da6a27364c15a3386f
IKrehan <irvigkrehan@hotmail.com>
2020-06-18 12:40:44 -0300

    Modificação páginas de cadastro/login

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ def homepage():
 def encomenda():
     return render_template('encomenda.html')
 
+@app.route('/registrar')
+def registrar():
+    return render_template('Register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
